55d28cd9b52088ae2b71f23d3b29cedb05320620
Declan Doyle <Declan@pc-204-49.customer.dundee.opal.lan>
2017-10-30 14:20:38 +0000

    Using jQuery to create animations on the events page

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-//alert("JavaScript Test!");
+$(document).ready(function(){
+  $("#marches").click(function() {
+    if($(this).hasClass("clicked")){
+      $(this).removeClass("clicked")
+
+      $(this).animate({left: '0'}, "slow", function() {$(this).removeAttr('style'); });
+    }else{
+      $(this).addClass("clicked");
+
+      $(this).animate({right: '106.5%'}, 'slow');
+    }
+  });
+
+
+  $("#palace").click(function() {
+    if($(this).hasClass("clicked")){
+      $(this).removeClass("clicked")
+
+      $(this).animate({left: '0'}, "slow", function() {$(this).removeAttr('style');} );
+    }else{
+      $(this).addClass("clicked");
+
+      $(this).animate({left: '106.5%'}, 'slow');
+    }
+  });
+
+  $("#gala").click(function() {
+    if($(this).hasClass("clicked")){
+      $(this).removeClass("clicked")
+
+      $(this).animate({left: '0'}, "slow", function() {$(this).removeAttr('style'); });
+    }else{
+      $(this).addClass("clicked");
+
+      $(this).animate({right: '106.5%'}, 'slow');
+    }
+  });
+
+  $("#jousting").click(function() {
+    if($(this).hasClass("clicked")){
+      $(this).removeClass("clicked")
+
+      $(this).animate({left: '0'}, "slow", function() {$(this).removeAttr('style');} );
+    }else{
+      $(this).addClass("clicked");
+
+      $(this).animate({left: '106.5%'}, 'slow');
+    }
+  });
+});
